2f5f4d9bfca530c886b3ea90ded68885c901c8b6
sam.critchley <sam.critchley@kiandra.com.au>
2015-03-04 00:25:26 +1100

    add replaced the strength calculation with an entropy based approach

--- app/scripts/directives/ng-password-strength.js
+++ app/scripts/directives/ng-password-strength.js
@@ -7,177 +7,94 @@
    * @description
    * # ngPasswordStrength
    */
-  angular.module('ngPasswordStrength', ['ngLodash'])
-    .directive('ngPasswordStrength', ['lodash', function(_) {
+  angular.module('ngPasswordStrength', [])
+    .directive('ngPasswordStrength', function() {
+      //TODO: make a provider for the directive or a service so these sets can be extended by the consumer
+      function add(label, exp, score) {
+        sets.push({label : label, regex: exp, score: score});
+      }
+
+      var sets = [];
+
+      add('ASCII Lowercase', /[a-z]/, 26);
+      add('ASCII Uppercase', /[A-Z]/, 26);
+      add('ASCII Numbers', /\d/, 10);
+      add('ASCII Top Row Symbols', /[!@£#\$%\^&\*\(\)\-_=\+]/, 15);
+      add('ASCII Other Symbols', /[\?\/\.>\,<`~\\|"';:\]\}\[\{\s]/, 19);
+
+      // Unicode Latin Subset
+      add('Unicode Latin 1 Supplement', /[\u00A1-\u00FF]/, 94);
+      add('Unicode Latin Extended A', /[\u0100-\u017F]/, 128);
+      add('Unicode Latin Extended B', /[\u0180-\u024F]/, 208);
+      add('Unicode Latin Extended C', /[\u2C60-\u2C7F]/, 32);
+      add('Unicode Latin Extended D', /[\uA720-\uA7FF]/, 29);
+
+      // Unicode Cyrillic Subset
+      add('Unicode Cyrillic Uppercase', /[\u0410-\u042F]/, 32);
+      add('Unicode Cyrillic Lowercase', /[\u0430-\u044F]/, 32);
+
       return {
-        template: '<div class="progress"><div class="progress-bar progress-bar-{{class}}" role="progressbar" aria-valuenow="{{value}}" aria-valuemin="0" aria-valuemax="100" ng-style="{width : ( value + \'%\' ) }"><span class="sr-only">{{value}}% Complete</span></div></div>',
-        restrict: 'A',
+        template: '<div class="progress"><div class="progress-bar progress-bar-{{class}}" role="progressbar" aria-valuenow="{{value}}" aria-valuemin="0" aria-valuemax="100" ng-style="{width : ( value + \'%\' ) }"><span class="sr-only">{{value}}% Secure</span></div></div>',
+        restrict: 'AE',
         scope: {
-          pwd: '=ngPasswordStrength',
-          value: '=strength'
+          password: '=ngPasswordStrength',
+          value: '=strength',
+          goal: '@?goal'
         },
-        link: function(scope /*, elem, attrs*/ ) {
-          var
-            measureStrength = function(p) {
-              var stringReverse = function(str) {
-                  for (var i = str.length - 1, out = ''; i >= 0; out += str[i--]) {}
-                  return out;
-                },
-                matches = {
-                  pos: {},
-                  neg: {}
-                },
-                counts = {
-                  pos: {},
-                  neg: {
-                    seqLetter: 0,
-                    seqNumber: 0,
-                    seqSymbol: 0
-                  }
-                },
-                tmp,
-                strength = 0,
-                letters = 'abcdefghijklmnopqrstuvwxyz',
-                numbers = '01234567890',
-                symbols = '\\!@#$%&/()=?¿',
-                back,
-                forth,
-                i;
+        link: function($scope /*, elem, attrs*/ ) {
+          var goal = parseInt($scope.goal || '96');
 
-              if (p) {
-                // Benefits
-                matches.pos.lower = p.match(/[a-z]/g);
-                matches.pos.upper = p.match(/[A-Z]/g);
-                matches.pos.numbers = p.match(/\d/g);
-                matches.pos.symbols = p.match(/[$-/:-?{-~!^_`\[\]]/g);
-                matches.pos.middleNumber = p.slice(1, -1).match(/\d/g);
-                matches.pos.middleSymbol = p.slice(1, -1).match(/[$-/:-?{-~!^_`\[\]]/g);
+          function calculateEntropy(password) {
+            var characters = 0;
 
-                counts.pos.lower = matches.pos.lower ? matches.pos.lower.length : 0;
-                counts.pos.upper = matches.pos.upper ? matches.pos.upper.length : 0;
-                counts.pos.numbers = matches.pos.numbers ? matches.pos.numbers.length : 0;
-                counts.pos.symbols = matches.pos.symbols ? matches.pos.symbols.length : 0;
-
-                tmp = _.reduce(counts.pos, function(memo, val) {
-                  // if has count will add 1
-                  return memo + Math.min(1, val);
-                }, 0);
-
-                counts.pos.numChars = p.length;
-                tmp += (counts.pos.numChars >= 8) ? 1 : 0;
-
-                counts.pos.requirements = (tmp >= 3) ? tmp : 0;
-                counts.pos.middleNumber = matches.pos.middleNumber ? matches.pos.middleNumber.length : 0;
-                counts.pos.middleSymbol = matches.pos.middleSymbol ? matches.pos.middleSymbol.length : 0;
-
-                // Deductions
-                matches.neg.consecLower = p.match(/(?=([a-z]{2}))/g);
-                matches.neg.consecUpper = p.match(/(?=([A-Z]{2}))/g);
-                matches.neg.consecNumbers = p.match(/(?=(\d{2}))/g);
-                matches.neg.onlyNumbers = p.match(/^[0-9]*$/g);
-                matches.neg.onlyLetters = p.match(/^([a-z]|[A-Z])*$/g);
-
-                counts.neg.consecLower = matches.neg.consecLower ? matches.neg.consecLower.length : 0;
-                counts.neg.consecUpper = matches.neg.consecUpper ? matches.neg.consecUpper.length : 0;
-                counts.neg.consecNumbers = matches.neg.consecNumbers ? matches.neg.consecNumbers.length : 0;
-
-
-                // sequential letters (back and forth)
-                for (i = 0; i < letters.length - 2; i++) {
-                  var p2 = p.toLowerCase();
-                  forth = letters.substring(i, parseInt(i + 3));
-                  back = stringReverse(forth);
-                  if (p2.indexOf(forth) !== -1 || p2.indexOf(back) !== -1) {
-                    counts.neg.seqLetter++;
-                  }
-                }
-
-                // sequential numbers (back and forth)
-                for (i = 0; i < numbers.length - 2; i++) {
-                  forth = numbers.substring(i, parseInt(i + 3));
-                  back = stringReverse(forth);
-                  if (p.indexOf(forth) !== -1 || p.toLowerCase().indexOf(back) !== -1) {
-                    counts.neg.seqNumber++;
-                  }
-                }
-
-                // sequential symbols (back and forth)
-                for (i = 0; i < symbols.length - 2; i++) {
-                  forth = symbols.substring(i, parseInt(i + 3));
-                  back = stringReverse(forth);
-                  if (p.indexOf(forth) !== -1 || p.toLowerCase().indexOf(back) !== -1) {
-                    counts.neg.seqSymbol++;
-                  }
-                }
-
-                // repeated chars
-                counts.neg.repeated = _.chain(p.toLowerCase().split('')).
-                countBy(function(val) {
-                    return val;
-                  })
-                  .reject(function(val) {
-                    return val === 1;
-                  })
-                  .reduce(function(memo, val) {
-                    return memo + val;
-                  }, 0)
-                  .value();
-
-                // Calculations
-                strength += counts.pos.numChars * 4;
-                if (counts.pos.upper) {
-                  strength += (counts.pos.numChars - counts.pos.upper) * 2;
-                }
-                if (counts.pos.lower) {
-                  strength += (counts.pos.numChars - counts.pos.lower) * 2;
-                }
-                if (counts.pos.upper || counts.pos.lower) {
-                  strength += counts.pos.numbers * 4;
-                }
-                strength += counts.pos.symbols * 6;
-                strength += (counts.pos.middleSymbol + counts.pos.middleNumber) * 2;
-                strength += counts.pos.requirements * 2;
-
-                strength -= counts.neg.consecLower * 2;
-                strength -= counts.neg.consecUpper * 2;
-                strength -= counts.neg.consecNumbers * 2;
-                strength -= counts.neg.seqNumber * 3;
-                strength -= counts.neg.seqLetter * 3;
-                strength -= counts.neg.seqSymbol * 3;
-
-                if (matches.neg.onlyNumbers) {
-                  strength -= counts.pos.numChars;
-                }
-                if (matches.neg.onlyLetters) {
-                  strength -= counts.pos.numChars;
-                }
-                if (counts.neg.repeated) {
-                  strength -= (counts.neg.repeated / counts.pos.numChars) * 10;
-                }
+            for(var i = 0; i< sets.length; i++){
+              var match = password.match(sets[i].regex);
+              if (match){
+                characters += sets[i].score;
               }
+            }
 
-              return Math.max(0, Math.min(100, Math.round(strength)));
-            },
+            var entropy = (Math.log(characters) / Math.LN2) * password.length;
+            return entropy;
+          }
 
+          function strengthAsMeasureOfGoal(entropy, goal) {
+            var goalAchieved = entropy / goal;
+            if (goalAchieved > 1){
+              goalAchieved = 100;
+            }
+            else{
+              goalAchieved = Math.round(goalAchieved * 100);
+            }
 
-            getClass = function(s) {
-              switch (Math.round(s / 33)) {
-                case 0:
-                case 1:
-                  return 'danger';
-                case 2:
-                  return 'warning';
-                case 3:
-                  return 'success';
-              }
-            };
+            return goalAchieved;
+          }
 
+          function getClass(s) {
+            switch (Math.round(s / 33)) {
+              case 0:
+              case 1:
+                return 'danger';
+              case 2:
+                return 'warning';
+              case 3:
+                return 'success';
+            }
+          }
 
-          scope.$watch('pwd', function() {
-            scope.value = measureStrength(scope.pwd);
-            scope.class = getClass(scope.value);
+          $scope.$watch('password', function(password) {
+            if (password && password.length) {
+              var entropy = calculateEntropy(password);
+              var goalAchievement = strengthAsMeasureOfGoal(entropy, goal);
+              $scope.value = goalAchievement;
+              $scope.class = getClass(goalAchievement);
+            }
+            else {
+              $scope.value = 0;
+              $scope.class = getClass(0);
+            }
           });
         },
       };
-    }]);
+    });
 })();
